c3731c290ab02ee14015032fa0e9dba432bc8163
Emildawood123 <66915790+Emildawood123@users.noreply.github.com>
2024-04-10 12:44:33 -0400

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,6 @@ encrypt_password
 import bcrypt
 from typing import ByteString
 
-
 def hash_password(password: str) -> ByteString:
     """hash_password method"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
