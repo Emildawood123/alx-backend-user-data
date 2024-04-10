ea91dca647897013412f87ba6f44fbab8c41c633
Emildawood123 <edawood997@gmail.com>
2024-04-10 12:57:19 -0400

    d

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""
-encrypt_password
-"""
+"""encrypt_password"""
 import bcrypt
 from typing import ByteString
+
 
 def hash_password(password: str) -> ByteString:
     """hash_password method"""
