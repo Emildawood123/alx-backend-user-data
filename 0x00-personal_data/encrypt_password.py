6d77d4e11db40a218d11db649e21316eab5e0efb
Emildawood123 <66915790+Emildawood123@users.noreply.github.com>
2024-04-10 12:43:10 -0400

    Create encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+encrypt_password
+"""
+import bcrypt
+from typing import ByteString
+
+
+def hash_password(password: str) -> ByteString:
+    """hash_password method"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
