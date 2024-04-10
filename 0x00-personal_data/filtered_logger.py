f480a04edb27068a7a3d3a405a8404e83e39af09
Emildawood123 <edawood997@gmail.com>
2024-04-10 08:55:37 -0400

    first with regular solve by gbt

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """filter_datum"""
     for field in fields:
-        message = re.sub(rf'(?<={field}=)([^{separator}]+)(?=;)',
-                         redaction, message)
+        message: str = re.sub(rf'(?<={field}=)([^{separator}]+)(?=;)',
+                              redaction, message)
     return message
