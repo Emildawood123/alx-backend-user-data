92e06116988b2b83850f2c09b4dc133f0abe8bb1
Emildawood123 <edawood997@gmail.com>
2024-04-10 09:30:28 -0400

    forget add property

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """filter_datum for hide the sen.. data"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
         message: str = re.sub(rf'(?<={field}=)([^{separator}]+)(?=;)',
                               redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format with previous funciton task"""
+        record.msg: str = filter_datum(self.fields, self.REDACTION,
+                                       record.msg, self.SEPARATOR)
+        return super().format(record)
