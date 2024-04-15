e069c18fed1c10160632af085f628af6addf491e
emil <edawood997@gmail.com>
2024-04-15 18:16:19 +0200

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,21 @@ class BasicAuth(Auth):
             return r.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """extract_user_credentials method"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) != str:
+            return None, None
+        if decoded_base64_authorization_header.find(':') == -1:
+            return None, None
+        else:
+            if len(decoded_base64_authorization_header) == 1:
+                return '', ''
+            else:
+                my_lst = decoded_base64_authorization_header.split(':')
+                user = my_lst[0]
+                passwd = my_lst[1]
+                return user, passwd
