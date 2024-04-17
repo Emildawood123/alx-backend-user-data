2c092b67e0dceb14598a0678ba693a165531d15e
emil <edawood997@gmail.com>
2024-04-17 18:30:23 +0200

    d

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -7,8 +7,7 @@ import base64
 from models.user import User
 import hashlib
 from os import getenv
-import datetime
-
+from datetime import timedelta, datetime
 
 class SessionExpAuth(SessionAuth):
     """SessionExpAuth class"""
@@ -29,3 +28,22 @@ class SessionExpAuth(SessionAuth):
             return session_id
         except Exception:
             return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user_id_for_session_id"""
+        if session_id is None:
+            return None
+        session_dictionary = self.user_id_by_session_id[session_id]
+        if session_dictionary is None:
+            return None
+        if self.session_duration <= 0:
+            return session_dictionary['user_id']
+        if "created_at" not in session_dictionary:
+            return None
+        created_at = session_dictionary['created_at']
+        if created_at is None:
+            return None
+        if (created_at + timedelta(seconds=self.session_duration)) \
+                < datetime.now():
+            return None
+        return session_dictionary['user_id']
