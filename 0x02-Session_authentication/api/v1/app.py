838ca46e8a5d5c3b201f63ca4a3beb2519f19316
emil <edawood997@gmail.com>
2024-04-17 17:52:04 +0200

    d

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,8 +52,8 @@ def before_request() -> None:
     res = auth.current_user(request)
     request.current_user = res
     my_lst = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
-        return abort(401)
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
     if auth is None:
         return
     if auth.require_auth(request.path, my_lst) is False:
