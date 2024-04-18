51ed63c6b0457c46c219e7f2203b1a44bb2c73ae
Emildawood123 <edawood997@gmail.com>
2024-04-18 13:36:13 -0400

    done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,13 +19,13 @@ class SessionAuth(Auth):
             return None
         else:
             session_id = uuid.uuid4()
-            user_id_by_session_id[session_id] = user_id
+            self.user_id_by_session_id[session_id] = user_id
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """user_is_for_session_id method"""
         if bool(isinstance(session_id, str)):
-            return user_id_by_session_id.get(session_id)
+            return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """current_user overload method"""
