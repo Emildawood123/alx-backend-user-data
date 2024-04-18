415a75a3bd80e61fb27e62fc8cbcb622b3446e54
Emildawood123 <edawood997@gmail.com>
2024-04-18 13:16:38 -0400

    expect when intparse is not work

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -13,13 +13,13 @@ from datetime import timedelta, datetime
 class SessionExpAuth(SessionAuth):
     """SessionExpAuth class"""
     def __init__(self):
-        if getenv('SESSION_DURATION'):
-            try:
+        try:
+            if getenv('SESSION_DURATION'):
                 self.session_duration = int(getenv('SESSION_DURATION'))
-            except Exception:
-                self.session_auth = 0
-        else:
-            self.session_duration = 0
+            else:
+                self.session_duration = 0
+        except Exception:
+            self.session_auth = 0
 
     def create_session(self, user_id=None):
         """create_session method"""
