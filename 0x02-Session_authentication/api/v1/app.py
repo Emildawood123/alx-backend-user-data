42a6dba37be00bf1b9086ee8c7ae688b64487fad
Emildawood123 <edawood997@gmail.com>
2024-04-19 21:35:42 -0400

    not

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,7 +58,8 @@ def before_request() -> None:
               '/api/v1/auth_session/login/']
     is_require = auth.require_auth(request.path, my_lst)
     if is_require:
-        if auth.authorization_header(request) and auth.session_cookie(request):
+        if not auth.authorization_header(request) and \
+                not auth.session_cookie(request):
             return None, abort(401)
         if auth.current_user(request) is None:
             return None, abort(403)
