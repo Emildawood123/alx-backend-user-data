c0d1e7868f90deb9744be0f29f68c454d46cf9b7
Emildawood123 <edawood997@gmail.com>
2024-04-19 23:02:48 -0400

    semi solution in task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """SessionAuth moudle"""
-from flask import request
+from flask import request, jsonify
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import base64
 from models.user import User
 import hashlib
 import uuid
+from api.v1.views import app_views
 
 
 class SessionAuth(Auth):
@@ -36,3 +37,28 @@ class SessionAuth(Auth):
         cookie = self.session_cookie(request)
         user_id = self.user_id_for_session_id(cookie)
         return User.get(user_id)
+
+    @app_views.route('/auth_session/login', methods=['POST'],
+                     strict_slashes=False)
+    def session_route() -> str:
+        try:
+            email = request.form.get('email')
+            if email == '' or email is None:
+                return jsonify({"error": "email missing"}), 400
+        except Exception:
+            return jsonify({"error": "email missing"}), 400
+        try:
+            password = request.form.get('password')
+            if password == '' or password is None:
+                return jsonify({"error": "password missing"}), 400
+        except Exception:
+            return jsonify({"error": "password missing"}), 400
+        get_user = User.search({'email': email})
+        if get_user == []:
+            return jsonify({"error": "no user found for this email"}), 404
+        else:
+            if not get_user[-1].is_valid_password(password):
+                return jsonify({"error": "wrong password"}), 401
+            else:
+                from api.v1.app import auth
+                return (get_user[-1]).to_json()
