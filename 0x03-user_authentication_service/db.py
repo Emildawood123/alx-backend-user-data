e9c2c5353a7b6a801424e14bd4d06624eb3ad64a
Emildawood123 <edawood997@gmail.com>
2024-04-23 00:17:29 -0400

    other tyr

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,8 +36,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """add_user for add user in db method"""
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        DB._session.add(new_user)
+        DB._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
