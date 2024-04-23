684f541033a0fba627c13757e1b364739b49492c
Emildawood123 <edawood997@gmail.com>
2024-04-23 10:51:47 -0400

    test by captilize the comment

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,7 +8,7 @@ from user import User
 
 
 def _hash_password(password: str) -> bytes:
-    """_hash_password method"""
+    """Returns hashpassword by password."""
     aft_hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return aft_hashed
 
