f99b02e4d667e5e35289a51a0c7cf20e8af9cb17
Emildawood123 <edawood997@gmail.com>
2024-04-23 00:33:17 -0400

    fuck i don't where the error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,11 +34,10 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add_user for add user in db method"""
+        """Add a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
-        self_session.rollback()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
