ce10f94fdbad9cd77c1e5cbcc494a6afb2df43cb
Emildawood123 <edawood997@gmail.com>
2024-04-22 23:16:42 -0400

    simple edit in annotaiton

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> Base:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """add_user method"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
