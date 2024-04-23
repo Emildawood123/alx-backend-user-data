4ee81cdaba410eedee47c0b098f27a5ead09e6db
Emildawood123 <edawood997@gmail.com>
2024-04-23 00:15:38 -0400

    just fuckin try

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add_user method"""
+        """add_user for add user in db method"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
