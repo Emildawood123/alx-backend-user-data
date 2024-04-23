581778cf7a9b7c3e9fb4b7f8a9eb08bf920d7ef4
Emildawood123 <edawood997@gmail.com>
2024-04-23 11:27:11 -0400

    lsa

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,3 +11,62 @@ def _hash_password(password: str) -> bytes:
     """_hash_password method that return bytes."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+
+def _generate_uuid() -> str:
+    """_generate_uuid method."""
+    return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """__init__ consturctor."""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> bytes:
+        """register_user method."""
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """valid_login method."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """create_session method."""
+        try:
+            user = self._db.find_user_by(email=email)
+            user.session_id = _generate_uuid()
+            return user.session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """get_user_from_session_id method."""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(user_id: int) -> None:
+        """destroy_session method."""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            user.session_id = None
+        except NoResultFound:
+            return None
