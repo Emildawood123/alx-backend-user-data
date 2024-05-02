9907e3fba7557f9e6c8e59805a6a6c32a30c5122
Emildawood123 <66915790+Emildawood123@users.noreply.github.com>
2024-05-02 17:59:19 +0300

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,4 +8,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """is_valid method"""
-    return bcrypt.checkpw(password, hashed_password)
+    if bcrypt.checkpw(password, hashed_password):
+        return True
+    return False
